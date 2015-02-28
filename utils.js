dd8b2f5c5db1b7e0b3b94fff21ce1c8a54f50894
Kiril Menshikov <kiril@kiril.me>
2015-02-28 23:41:35 +0200

    utils

---
+++ utils.js
@@ -0,0 +1,91 @@
+
+var toString = Object.prototype.toString;
+
+exports.isObject = function (arg) {
+  return '[object Object]' == toString.call(arg);
+}
+
+exports.clone = function clone (obj, options) {
+  if (obj === undefined || obj === null)
+    return obj;
+
+  if (Array.isArray(obj))
+    return cloneArray(obj, options);
+
+  if (obj.constructor) {
+    switch (obj.constructor.name) {
+      case 'Object':
+        return cloneObject(obj, options);
+      case 'Date':
+        return new obj.constructor(+obj);
+      // case 'RegExp':
+      //   return cloneRegExp(obj);
+      default:
+        // ignore
+        break;
+    }
+  }
+
+  // if (obj instanceof ObjectId)
+  //   return new ObjectId(obj.id);
+
+  if (!obj.constructor && exports.isObject(obj)) {
+    // object created with Object.create(null)
+    return cloneObject(obj, options);
+  }
+
+  if (obj.valueOf)
+    return obj.valueOf();
+};
+var clone = exports.clone;
+
+function cloneArray (arr, options) {
+  var ret = [];
+  for (var i = 0, l = arr.length; i < l; i++)
+    ret.push(clone(arr[i], options));
+  return ret;
+};
+
+function cloneObject (obj, options) {
+  var retainKeyOrder = options && options.retainKeyOrder
+    , minimize = options && options.minimize
+    , ret = {}
+    , hasKeys
+    , keys
+    , val
+    , k
+    , i
+
+  if (retainKeyOrder) {
+    for (k in obj) {
+      val = clone(obj[k], options);
+
+      if (!minimize || ('undefined' !== typeof val)) {
+        hasKeys || (hasKeys = true);
+        ret[k] = val;
+      }
+    }
+  } else {
+    // faster
+
+    keys = Object.keys(obj);
+    i = keys.length;
+
+    while (i--) {
+      k = keys[i];
+      val = clone(obj[k], options);
+
+      if (!minimize || ('undefined' !== typeof val)) {
+        if (!hasKeys) hasKeys = true;
+        ret[k] = val;
+      }
+    }
+  }
+
+  return minimize
+    ? hasKeys && ret
+    : ret;
+};
+
+
+
